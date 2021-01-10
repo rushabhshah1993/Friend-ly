9ac035c513c387539a0251ee970b428e8b725f39
Rushabh Shah <shahrushabh1993@gmail.com>
2021-01-11 01:57:00 +0530

    Adding the logo

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'public', 'index.html'),
-            filename: 'index.html'
+            filename: 'index.html',
+            favicon: './src/assets/images/logo.png'
         })
     ],
     devServer: {
